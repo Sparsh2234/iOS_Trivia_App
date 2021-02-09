ced8353e38b0c068ca61c6088de0d46dc6037861
sparsh2234 <sparsh1411@gmail.com>
2021-02-08 22:29:59 -0500

    initial commit

---
+++ Model/Question.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+struct Question {
+    var text:String
+    var answer:String
+    
+    init(q:String, a:String) {
+        text = q
+        answer = a
+    }
+    
+    
+}
